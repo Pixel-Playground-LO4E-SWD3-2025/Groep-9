5474d5f2502cbf5fdd9d6ca306f307bf096dae70
piet <6044689@mborijnland.nl>
2025-05-14 10:23:36 +0200

    overons een begin gemaakt
    een begginetje gemaakt in over ons

--- overons.php
+++ overons.php
@@ -1,2 +1,19 @@
-<?php require_once 'footer.php'; ?>
 <?php require_once 'header.php'; ?>
+<body>
+    <main>
+     <section>
+        <article>
+          <h1 id="welkom">Menu</h1>
+            <video autoplay loop muted src = "video/Skycolor.mp4"></video>
+       </article>
+
+        <article class="Block">
+          <P id="Head">Wie zijn wij</p>
+       </article>
+       <article>
+        
+       </article>
+     </section>
+    <main>
+</body>
+<?php require_once 'footer.php';?>
