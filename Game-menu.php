fb9dddd31c110981ca270c42d19ea186370e992b
6047306-David <6047306@mborijnland.nl>
2025-05-14 11:57:32 +0200

    verder gegaan met de game menu pagina
    en een bestand naam veranderd

--- Game-menu.php
+++ Game-menu.php
@@ -6,9 +6,14 @@
             <article>
             <h1 class="welkom">Kies je Game</h1>
           </article>
-            <article class="">
-            </article>
-        </section>
+              <article class="Game-block">
+                 <p class="Gamenaam">Steen-Papier-Schaar</p>
+                 <img class="Game-img" src="img/Steen-Papier-Schaar.png" alt="Steen-Papier-Schaar">
+                 <p class="Gameinfo">Steen-Papier-Schaar is een eenvoudig maar leuke game tegen de computer, 
+                  het is een geweldige manier om je reflexen te testen kan jij hem verslaan?!</p>
+                  <button class="ingang" onclick="location.href='Steen-papier-schaar.php'">Play Games</button>
+              </article>
+        </section>    
 </main>
 </body>
 
