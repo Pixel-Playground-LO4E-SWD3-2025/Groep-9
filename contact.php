3df1f2cb245e3dd83b455882fe85635d703d0503
piet <6044689@mborijnland.nl>
2025-05-27 14:00:07 +0200

    beginnetje aan de contact pagina gemaakt.

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php require_once 'header.php'; ?>
+<body>
+    <main>
+      <section> 
+           <video class="skycolor" autoplay loop muted src = "video/Skycolor.mp4"></video> 
+    </section>
+</body>
+<?php require_once 'footer.php'; ?>
