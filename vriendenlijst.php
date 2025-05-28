4afe11f6f3e86f8baac091e178cd0ac313f0e9af
piet <6044689@mborijnland.nl>
2025-05-28 10:31:09 +0200

    Update vriendenlijst.php

--- vriendenlijst.php
+++ vriendenlijst.php
@@ -1,6 +1,14 @@
 <?php require_once 'header.php'; ?>
 <body>
-    
+    <main>
+        <video class="skycolor " autoplay looped muted src="video/skycolor.mp4"></video>
+        <section>
+            <article class="vriendenlijst">
+                <h1 class="welkom">Vriendenlijst</h1>
+                <p class="info">Hier
+            </article>
+        </section>
+    </main>
 </body>
 
 <?php require_once 'footer.php'; ?>
