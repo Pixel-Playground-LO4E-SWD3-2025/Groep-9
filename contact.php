cf5f321fb8f1598282c2be74760ff5c15db90c30
piet <6044689@mborijnland.nl>
2025-05-28 10:15:24 +0200

    bezig met contacvt pagina

--- contact.php
+++ contact.php
@@ -3,6 +3,17 @@
     <main>
       <section> 
            <video class="skycolor" autoplay loop muted src = "video/Skycolor.mp4"></video> 
+           <article>
+                            <form action="login_process.php" methode="POST">
+                     <label for ="Username">Naam:</label>
+                     <input type="text" id="username" name="username" required>
+                     <label for ="email">E-mail: </label>
+                     <input type="Email" id="email" name="email" required>
+                     <label for="password">Wat voor reden:</label>
+                     <input type="password" id="password" name="password" required>
+                     <button id="inlogbutton" type ="submit">Login</button>
+                </form>
+           </article>
     </section>
 </body>
 <?php require_once 'footer.php'; ?>
