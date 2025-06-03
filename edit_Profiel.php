43fea875e81e2cc1daeb7ef684cee679cc17af65
6047306-David <6047306@mborijnland.nl>
2025-06-03 15:07:33 +0200

    nog niet af met de edit profiel pagina

--- edit_Profiel.php
+++ edit_Profiel.php
@@ -1,9 +1,29 @@
 <?php require_once 'header.php'; ?>
+  <?php
+    session_start();
+    include_once 'connection.php';
+    
+    $user_id = $_SESSION['user_id'];
+    $query = "SELECT * FROM users WHERE id = $user_id";
+    $result = $conn->query($query);
+    $user = $result -> fetch(PDO::FETCH_ASSOC);
+    ?>
+    <form action = "Profiel.php" method="POST">
+      <label>Name:</label>
+      <input type="text" name="name" value="<?php echo htmlspecialchars($user['name']); ?>" required><br>
+      <label>Email:</label>
+      <input type="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required><br>
+
+      <button type="submit">update Profile</button>
+
+   
    <body>
      <main>
         <section>
             <video class="skycolor" autoplay loop muted src="video/skycolor.mp4"></video>
             <article class="profielpagina">
+
+
              
             </article>
       </section>    
