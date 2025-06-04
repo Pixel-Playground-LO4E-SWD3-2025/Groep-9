47ed62df56a0d021da729df1fb1a6a6f0390b2d2
6047306-David <6047306@mborijnland.nl>
2025-06-04 09:38:31 +0200

    nog niet klaar

--- edit_Profiel.php
+++ edit_Profiel.php
@@ -1,18 +1,22 @@
 <?php require_once 'header.php'; ?>
+  
   <?php
-    session_start();
+  
     include_once 'connection.php';
     
     $user_id = $_SESSION['user_id'];
-    $query = "SELECT * FROM users WHERE id = $user_id";
-    $result = $conn->query($query);
-    $user = $result -> fetch(PDO::FETCH_ASSOC);
+    $query = "SELECT * FROM users WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt-> execute([$user_id]);
+    $user = $stmt-> fetch(PDO::FETCH_ASSOC);
     ?>
     <form action = "Profiel.php" method="POST">
       <label>Name:</label>
-      <input type="text" name="name" value="<?php echo htmlspecialchars($user['name']); ?>" required><br>
+      <input type="text" name="name" value="<?php echo htmlspecialchars($user['username']); ?>" required><br>
       <label>Email:</label>
       <input type="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required><br>
+      <label>Password:</label>
+      <input type="password" name=password value="" placeholder="Enter new password for change"><br>
 
       <button type="submit">update Profile</button>
 
